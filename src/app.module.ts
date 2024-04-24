f7ea21480ba34bfbb892106152d7f0c853c9bd75
michu2k <michal.strumpf@gmail.com>
2024-04-24 10:34:12 +0200

    Added environments validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import {Module} from "@nestjs/common";
+import {ConfigModule} from "@nestjs/config";
 import {PrismaModule} from "./prisma/prisma.module";
 import {ServiceModule} from "./service/service.module";
 import {LocationModule} from "./location/location.module";
 import {BookingModule} from "./booking/booking.module";
 import {UserModule} from "./user/user.module";
 import {AuthModule} from "./auth/auth.module";
+import {validateEnvs} from "./env.validation";
 
 @Module({
-  imports: [LocationModule, ServiceModule, PrismaModule, BookingModule, UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({validate: validateEnvs}),
+    LocationModule,
+    ServiceModule,
+    PrismaModule,
+    BookingModule,
+    UserModule,
+    AuthModule
+  ],
   controllers: [],
   providers: []
 })
---
+++ src/env.validation.ts
@@ -0,0 +1,34 @@
+import {plainToInstance} from "class-transformer";
+import {IsString, validateSync} from "class-validator";
+
+class EnvironmentVariables {
+  @IsString()
+  DATABASE_URL: string;
+
+  @IsString()
+  POSTGRES_PASSWORD: string;
+
+  @IsString()
+  POSTGRES_USER: string;
+
+  @IsString()
+  POSTGRES_DB: string;
+
+  @IsString()
+  GOOGLE_CLIENT_ID: string;
+
+  @IsString()
+  GOOGLE_CLIENT_SECRET: string;
+}
+
+export function validateEnvs(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config);
+
+  const errors = validateSync(validatedConfig, {skipMissingProperties: false});
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
