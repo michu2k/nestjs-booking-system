e910b75ce4de4274f6c19c4e4cff7e5e8fd92fa6
michu2k <michal.strumpf@gmail.com>
2024-05-02 17:48:47 +0200

    auth.module: Added JWT module & updated module configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,28 @@
 import {Module} from "@nestjs/common";
+import {JwtModule} from "@nestjs/jwt";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 import {PrismaModule} from "../prisma/prisma.module";
+import {UserModule} from "../user/user.module";
 import {AuthService} from "./auth.service";
 import {AuthController} from "./auth.controller";
+import {GoogleStrategy} from "./strategies/google.strategy";
 
 @Module({
-  imports: [PrismaModule],
-  providers: [AuthService],
+  imports: [
+    PrismaModule,
+    UserModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>("JWT_SECRET"),
+        signOptions: {expiresIn: "7d"}
+      }),
+      inject: [ConfigService]
+    })
+  ],
+  providers: [AuthService, GoogleStrategy],
   controllers: [AuthController],
   exports: [AuthService]
 })
