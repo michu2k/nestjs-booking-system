40747c56472459ec343548d9c4060f1a7de14fcc
michu2k <michal.strumpf@gmail.com>
2024-05-02 18:43:27 +0200

    auth: Added google.strategy

---
+++ src/auth/strategies/google.strategy.ts
@@ -0,0 +1,58 @@
+import {Injectable} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
+import {UserRole} from "@prisma/client";
+import {PassportStrategy} from "@nestjs/passport";
+import {Profile, Strategy} from "passport-google-oauth20";
+import {UserService} from "../../user/user.service";
+import {AuthService} from "../auth.service";
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    protected configService: ConfigService
+  ) {
+    super({
+      clientID: configService.get<string>("GOOGLE_CLIENT_ID"),
+      clientSecret: configService.get<string>("GOOGLE_CLIENT_SECRET"),
+      callbackURL: configService.get<string>("GOOGLE_CALLBACK_URL"),
+      scope: ["email", "profile"]
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: Profile) {
+    console.log("validate", {accessToken, refreshToken, profile});
+
+    // TODO: Get picture
+    const {emails, displayName, provider, id: providerId} = profile;
+
+    const email = emails[0].value;
+    const user = await this.userService.findOneUser({email});
+
+    // If user does not exist, create a new user account
+    if (!user) {
+      return await this.userService.createUserAccount({
+        name: displayName,
+        email,
+        role: UserRole.USER,
+        account: {
+          provider,
+          providerAccountId: providerId
+        }
+      });
+    }
+
+    const providerAccountExists = await this.authService.checkIfProviderAccountExists(user.id, profile);
+
+    if (!providerAccountExists) {
+      await this.userService.createSSOProviderAccount({
+        provider,
+        providerAccountId: providerId,
+        userId: user.id
+      });
+    }
+
+    return user;
+  }
+}
