e47cb4eba5feb439a061fb9f191aad66e1c7b879
michu2k <michal.strumpf@gmail.com>
2024-05-10 20:27:57 +0200

    auth.service: Added generateAuthTokens method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,18 @@
 import {Injectable} from "@nestjs/common";
 import {Profile} from "passport-google-oauth20";
+import {ConfigService} from "@nestjs/config";
 import {JwtService} from "@nestjs/jwt";
 import {PrismaService} from "../prisma/prisma.service";
 import {UserEntity} from "../user/user.dto";
-import {JwtPayload} from "./strategies/jwt.strategy";
+import {UserService} from "../user/user.service";
 
 @Injectable()
 export class AuthService {
   constructor(
     private prisma: PrismaService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private userService: UserService,
+    protected configService: ConfigService
   ) {}
 
   /**
@@ -31,5 +34,26 @@ export class AuthService {
   async generateAccessToken({id, email}: UserEntity) {
     const payload: JwtPayload = {sub: id, email};
     return await this.jwtService.signAsync(payload);
+  }
+
+  async generateAuthTokens({id, email}: UserEntity) {
+    const payload: JwtPayload = {sub: id, email};
+
+    const accessToken = await this.jwtService.signAsync(payload, {
+      expiresIn: this.configService.get("ACCESS_TOKEN_VALIDITY")
+    });
+
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      expiresIn: this.configService.get("REFRESH_TOKEN_VALIDITY")
+    });
+
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+
+    await this.userService.updateUserRefreshToken(id, hashedRefreshToken);
+
+    return {
+      accessToken,
+      refreshToken
+    };
   }
 }
