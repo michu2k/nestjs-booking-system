62a0213263af1aa2761c82bfb72fcc43a7066952
michu2k <michal.strumpf@gmail.com>
2024-05-13 14:30:06 +0200

    Fixed broken service tests

--- src/location/location.service.spec.ts
+++ src/location/location.service.spec.ts
@@ -1,4 +1,5 @@
 import {Test, TestingModule} from "@nestjs/testing";
+import {PrismaModule} from "../prisma/prisma.module";
 import {LocationService} from "./location.service";
 
 describe("LocationService", () => {
@@ -6,6 +7,7 @@ describe("LocationService", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [LocationService]
     }).compile();
 
--- src/service/service.service.spec.ts
+++ src/service/service.service.spec.ts
@@ -1,4 +1,5 @@
 import {Test, TestingModule} from "@nestjs/testing";
+import {PrismaModule} from "../prisma/prisma.module";
 import {ServiceService} from "./service.service";
 
 describe("ServiceService", () => {
@@ -6,6 +7,7 @@ describe("ServiceService", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [ServiceService]
     }).compile();
 
