ffe64c70fcf7dd08e0deaf3041edef316c6f19ff
michu2k <michal.strumpf@gmail.com>
2024-05-18 14:56:24 +0200

    service: Added controller tests

--- src/service/service.controller.spec.ts
+++ src/service/service.controller.spec.ts
@@ -1,19 +1,95 @@
 import {Test, TestingModule} from "@nestjs/testing";
 import {ServiceController} from "./service.controller";
-import {ServiceModule} from "./service.module";
+import {ServiceService} from "./service.service";
+import {CreateServiceDto, UpdateServiceDto} from "./service.dto";
+import {mockService} from "./service.mocks";
 
 describe("ServiceController", () => {
   let controller: ServiceController;
+  let serviceService: ServiceService;
+
+  const mockServiceService = {
+    findAllServices: jest.fn().mockResolvedValue([mockService, mockService]),
+    findOneService: jest.fn().mockResolvedValue(mockService),
+    createService: jest.fn((data: CreateServiceDto) => Promise.resolve({...mockService, ...data})),
+    updateService: jest.fn((_, data: UpdateServiceDto) => Promise.resolve({...mockService, ...data})),
+    deleteService: jest.fn().mockResolvedValue(mockService)
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ServiceModule]
+      providers: [
+        {
+          provide: ServiceService,
+          useValue: mockServiceService
+        }
+      ],
+      controllers: [ServiceController]
     }).compile();
 
     controller = module.get<ServiceController>(ServiceController);
+    serviceService = module.get<ServiceService>(ServiceService);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  it("serviceService should be defined", () => {
+    expect(serviceService).toBeDefined();
+  });
+
+  describe("findAll", () => {
+    it("should get all services", async () => {
+      const result = await controller.findOne(mockService.id);
+
+      expect(serviceService.findOneService).toHaveBeenCalled();
+      expect(result).toEqual(mockService);
+    });
+  });
+
+  describe("findOne", () => {
+    it("should get a service", async () => {
+      const result = await controller.findOne(mockService.id);
+
+      expect(serviceService.findOneService).toHaveBeenCalled();
+      expect(result).toEqual(mockService);
+    });
+  });
+
+  describe("create", () => {
+    it("should create a service", async () => {
+      const createServiceDto: CreateServiceDto = {
+        name: "New",
+        description: "Simple description",
+        price: 27,
+        locationId: 1
+      };
+      const result = await controller.create(createServiceDto);
+
+      expect(serviceService.createService).toHaveBeenCalled();
+      expect(result).toEqual({...mockService, ...createServiceDto});
+    });
+  });
+
+  describe("update", () => {
+    it("should update a service", async () => {
+      const updateServiceDto: UpdateServiceDto = {
+        description: "Updated description"
+      };
+      const mockUpdatedService = {...mockService, ...updateServiceDto};
+      const result = await controller.update(mockService.id, updateServiceDto);
+
+      expect(result).toEqual(mockUpdatedService);
+    });
+  });
+
+  describe("delete", () => {
+    it("should delete a service", async () => {
+      const result = await controller.delete(mockService.id);
+
+      expect(serviceService.deleteService).toHaveBeenCalled();
+      expect(result).toEqual(mockService);
+    });
   });
 });
