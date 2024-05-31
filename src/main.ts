45ee72707bc1f071cbe7eab5d32bb11a269bc924
michu2k <michal.strumpf@gmail.com>
2024-05-31 15:55:47 +0200

    main.ts: Updated swagger path

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api/v1", app, document);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(3000);
 }
