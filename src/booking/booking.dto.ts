ff6003e33faf84d471bf6d6181b226e470575383
michu2k <michal.strumpf@gmail.com>
2024-06-07 13:57:18 +0200

    booking.dto: Simplified Create & Update dtos

--- src/booking/booking.dto.ts
+++ src/booking/booking.dto.ts
@@ -2,26 +2,6 @@ import {ApiProperty, PartialType, PickType} from "@nestjs/swagger";
 import {Booking, BookingStatus} from "@prisma/client";
 import {IsDateString, IsEnum, IsInt} from "class-validator";
 
-export class CreateBookingDto {
-  @IsDateString({strict: true, strictSeparator: true})
-  from: Date;
-
-  @IsDateString({strict: true, strictSeparator: true})
-  to: Date;
-
-  @IsEnum(BookingStatus)
-  @ApiProperty({enum: [BookingStatus.CONFIRMED, BookingStatus.PENDING, BookingStatus.CANCELLED]})
-  status: BookingStatus;
-
-  @IsInt()
-  userId: number;
-
-  @IsInt()
-  serviceId: number;
-}
-
-export class UpdateBookingDto extends PartialType(PickType(CreateBookingDto, ["from", "to", "status"] as const)) {}
-
 export class BookingEntity implements Booking {
   @IsInt()
   id: number;
@@ -48,3 +28,7 @@ export class BookingEntity implements Booking {
   @IsInt()
   userId: number;
 }
+
+export class CreateBookingDto extends PickType(BookingEntity, ["from", "to", "status", "userId", "serviceId"]) {}
+
+export class UpdateBookingDto extends PartialType(PickType(BookingEntity, ["from", "to", "status"] as const)) {}
